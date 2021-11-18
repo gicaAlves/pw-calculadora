3c6aa928543d1e320e904ec95358ffb4772b3d49
Giovanna Alves <giov.alves@outlook.com>
2021-11-18 12:58:59 -0300

    Adicionar arquivo do script JS

---
+++ script.js
@@ -0,0 +1,19 @@
+ // Passo a passo das funcionalidades da calculadorinha:
+ // pegar o elemento do visor
+ var visor = document.getElementById("visor");
+
+ // quando uma tecla for pressionada, seu valor vai pro visor
+ function adicionae(tecla) {
+   visor.value = visor.value + tecla;
+ }
+
+ // quando o "c" for pressionado limpa o visor
+ function limpezatotal() {
+   visor.value = "";
+   alert("PRA CIMA! 🤪⏫");
+ }
+
+ // quando o "=" for pressionado calcula
+ function calculari() {
+   visor.value = eval(visor.value);
+ }
